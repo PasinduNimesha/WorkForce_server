7e38997fdb66b1a03313da869115b617e332b5c4
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-02-11 09:47:23 +0530

    added user controller

--- src/main/kotlin/com/workforcepro/server/controller/LoginController.kt
+++ src/main/kotlin/com/workforcepro/server/controller/LoginController.kt
@@ -1,4 +1,13 @@
 package com.workforcepro.server.controller
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.*
+
+@RestController
 class LoginController {
+    @PostMapping("/login")
+    fun login(@RequestBody body: Map<String, String>): String {
+        val message = "Login successful" + "\n Welcome ${body["username"]}"
+        return message
+    }
 }
