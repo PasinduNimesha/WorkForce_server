2aade3c4959353267aefe3a1a719c35d8a8921ab
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-02-11 10:18:37 +0530

    Update LoginController.kt

--- src/main/kotlin/com/workforcepro/server/controller/LoginController.kt
+++ src/main/kotlin/com/workforcepro/server/controller/LoginController.kt
@@ -2,12 +2,17 @@ package com.workforcepro.server.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import java.net.http.HttpResponse
 
 @RestController
 class LoginController {
     @PostMapping("/login")
-    fun login(@RequestBody body: Map<String, String>): String {
-        val message = "Login successful" + "\n Welcome ${body["username"]}"
-        return message
+    fun login(@RequestBody body: Map<String, String>): ResponseEntity<String> {
+        val message = "Login successful" + "\nWelcome ${body["username"]}"
+        //response set to 200
+
+        return ResponseEntity(message, HttpStatus.OK)
     }
 }
